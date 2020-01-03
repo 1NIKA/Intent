56ed4c27a87e60411bef237dbfe819dcdc3a9f8b
Your Name <you@example.com>
2020-01-03 14:37:10 +0400

    First commit

---
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gotoSecondBtn.setOnClickListener {
+
+
+            val sqesi = inputSqesi.text.toString()
+
+            val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("სქესი", sqesi)
+            startActivity(intent)
+
+            finish()
+        }
+    }
+}
